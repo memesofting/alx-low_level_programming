6c1d3f8376757fe0ba4e12d2b42eb1dd0b1cb8e3
memesofting <bibitayoadekunle00@gmail.com>
2023-07-19 19:04:27 +0000

    _putchar alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * entry - point to print alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+	return (0);
+}
