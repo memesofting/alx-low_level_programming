aa9c8264a03cbd6df40e9bb5f6c1e3a4ec403820
memesofting <bibitayoadekunle00@gmail.com>
2023-07-28 14:48:30 +0000

    compare and clear anty betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,12 +6,13 @@
  * @s2: second string
  *
  * Return: 0 if s1 = s2
- * else: 1 if s1 > s2
+ * else if: s1 > s2 0
  * else: -1
  */
 int _strcmp(char *s1, char *s2)
 {
 	int result;
+
 	result = strcmp(s1, s2);
 	return (result);
 }
