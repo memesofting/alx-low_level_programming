c4362f480b21480e88fecbe6484a9f5beb7f9100
memesofting <bibitayoadekunle00@gmail.com>
2023-07-28 13:36:35 +0000

    join strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: pointer (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
