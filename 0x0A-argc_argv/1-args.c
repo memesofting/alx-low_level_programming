af850a8a0310cd79c761ba2ebd55d093420ad76b
memesofting <bibitayoadekunle00@gmail.com>
2023-08-09 23:23:27 +0000

    print argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints name of program
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
