2c24908e6234393037846715469defd3643bea16
memesofting <bibitayoadekunle00@gmail.com>
2023-11-05 22:47:08 +0000

    grid with double pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,11 +1,37 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * alloc_grid - returns pointer to a 2d array of integers
+ * @width: array no of rows
+ * @height: array no of columns
  *
  * Return: 0 on success
  */
 
 int **alloc_grid(int width, int height)
 {
+	int **grid;
+	int i;
+	int j;
+
+	grid = malloc(sizeof(int *) * width);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < width; i++)
+	{
+		grid[i] = malloc(sizeof(int) * height);
+
+		for (j = 0; j < height; j++)
+		{
+			*grid[i] = 0;
+		}
+	}
+	return (grid);
 }
