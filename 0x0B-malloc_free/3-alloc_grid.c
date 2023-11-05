965d849791e2ef201fbec22f76ccbf3a25a616d8
memesofting <bibitayoadekunle00@gmail.com>
2023-11-05 19:37:54 +0000

    switching to vscode

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns pointer to a 2d array of integers
+ *
+ * Return: 0 on success
+ */
+
+int **alloc_grid(int width, int height)
+{
+}
--- 0x0B-malloc_free/join.c
+++ 0x0B-malloc_free/join.c
@@ -24,24 +24,27 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (s1);
 	}
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == 	NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
-	l1 = strlen(s1);
-	l2 = strlen(s2) + 1;
-	new = malloc(sizeof(*s1) * l1 + sizeof(*s2) * l2);
-	if (new == NULL)
+	else
 	{
-		return (NULL);
+		l1 = strlen(s1);
+		l2 = strlen(s2) + 1;
+		new = malloc(sizeof(*s1) * l1 + sizeof(*s2) * l2);
+		if (new == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < l1; i++)
+		{
+			new[i] = s1[i];
+		}
+		for (i = 0; i < l2; i++)
+		{
+			new[l1 + i] = s2[i];
+		}
+		return (new);
 	}
-	for (i = 0; i < l1; i++)
-	{
-		new[i] = s1[i];
-	}
-	for (i = 0; i < l2; i++)
-	{
-		new[l1 + i] = s2[i];
-	}
-	return (new);
 }
