e6234e0fe7c105d9388faf57a0e719a035d25cfb
memesofting <bibitayoadekunle00@gmail.com>
2023-11-07 11:44:37 +0000

    defined new type with struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog
+#define dog
+
+/**
+ * struct dog - dog details
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}my_dog;
+
+#endif
