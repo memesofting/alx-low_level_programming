87293452ff7ccb3683438f194e69178ec6e8e073
memesofting <bibitayoadekunle00@gmail.com>
2023-11-17 10:25:19 +0000

    checked for null string in list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,6 +16,12 @@ size_t print_list(const list_t *h)
 	n = 0;
 	while (h != NULL)
 	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			n++;
+		}
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		n++;
