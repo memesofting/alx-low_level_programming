52dc6e906b770d92ad514821883bfb8a28cecb3c
memesofting <bibitayoadekunle00@gmail.com>
2023-11-17 13:33:05 +0000

    added new node to list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - adds a new node at the beginning of list_t
+ * @head: singly linked list
+ * @str: string to be duplicated
+ *
+ * Return: sizeof list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	size_t n;
+
+	n = 0;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	n = _strlen(str);
+	new->len = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,6 +17,7 @@ typedef struct list_s
 } list_t;
 #include <stddef.h>
 
+int _strlen(const char *s);
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
