c0e6269b939d4c8e3e205130fb045034cf3fe0ee
memesofting <bibitayoadekunle00@gmail.com>
2023-11-20 20:28:36 +0000

    list elements of list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,6 +7,18 @@
  * Return: no of elements
  */
 
-size_t listint_len(const listint *h)
+size_t listint_len(const listint_t *h)
 {
+	int i;
+
+	i = 1;
+	if (h->next != NULL)
+	{
+		i++;
+		return (i);
+	}
+	else
+	{
+		return (i);
+	}
 }
