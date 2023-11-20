f93d832857827173862ede1b5f2fa5389581d050
memesofting <bibitayoadekunle00@gmail.com>
2023-11-20 18:27:17 +0000

    switching to phone

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns number of elements in a linked list
+ * @h: linked list
+ *
+ * Return: no of elements
+ */
+
+size_t listint_len(const listint *h)
+{
+}
