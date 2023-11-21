1bcc39bc232f2c22b7bc5aae3bb56622328a1032
memesofting <bibitayoadekunle00@gmail.com>
2023-11-21 08:20:18 +0000

    print number of elements corrected

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,24 +1,25 @@
 #include "lists.h"
 
 /**
- * listint_len - returns number of elements in a linked list
- * @h: linked list
+ * listint_len - prints number of elements of a list
+ * @h: struct pointer
  *
- * Return: no of elements
+ * Return: no of nodes
  */
 
 size_t listint_len(const listint_t *h)
 {
-	int i;
+	size_t i;
 
-	i = 1;
-	if (h->next != NULL)
+	i = 0;
+	if (h == NULL)
 	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		h = h->next;
 		i++;
-		return (i);
 	}
-	else
-	{
-		return (i);
-	}
+	return (i);
 }
