2f402b6ebea64fa1f6c0d0c3236c542008c04c03
memesofting <bibitayoadekunle00@gmail.com>
2023-11-22 20:31:51 +0000

    phone switching

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * sum_listint - prints sum of all data(n) of a list
+ * @head: linked list
+ *
+ * Return: sum on success
+ */
+
+int sum_listint(listint_t *head)
+{
+	if (head == NULL)
+	{
+		return (0);
+	}
+}
