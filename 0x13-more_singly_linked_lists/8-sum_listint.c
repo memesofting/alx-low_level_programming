3d65ffec0618d8f0a9b1a9c1fb827af8bb36bbe4
memesofting <bibitayoadekunle00@gmail.com>
2023-11-23 03:15:27 +0000

    sum data of linked list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,8 +9,17 @@
 
 int sum_listint(listint_t *head)
 {
+	int sum;
+
+	sum = 0;
 	if (head == NULL)
 	{
 		return (0);
 	}
+	while (head != NULL)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
 }
