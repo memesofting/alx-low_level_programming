c17b134022c75eff3d217c8ca6e031e7f97a2705
memesofting <bibitayoadekunle00@gmail.com>
2023-11-23 19:19:08 -0800

    binary to base 10

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * binary_to_uint - converts binary to unsigned int
@@ -9,16 +10,27 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, len, num;
+	unsigned int i, len, num, j, pow;
+	int base;
 
+	pow = 1;
+	num = 0;
+	base = 2;
 	if (b == NULL)
 		return (0);
-	while (*b != '\0')
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
+		return (b[0] - 48);
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		num = b[i] * (2 ** len);
-		i++;
-		num += num;
+		if (b[i] != '0' && b[i] != '1')
+		return (0);
+		for (j = len - 1; j > 0; j--)
+		pow = pow * base;
+		num = num + (pow * (b[i] - 48));
 		len--;
+		pow = 1;
 	}
 	return (num);
 }
