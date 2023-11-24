cc40d57b053b9cfe2aa1ae4b1b56d35408399799
memesofting <bibitayoadekunle00@gmail.com>
2023-11-24 11:14:33 -0800

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,13 @@
  * main - entry point
  * @i: type to return
  *
- * Return: 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
 	char *quote;
 
-	quote = "'and that piece of art is useful' - Dora Korpar, 2015-10-19";
+	quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	fwrite(quote, sizeof(char), strlen(quote), stderr);
-	return (0);
+	return (1);
 }
