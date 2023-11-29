28539191d83e6f38d8f7bcfd57141c9ffef91bea
memesofting <bibitayoadekunle00@gmail.com>
2023-11-29 22:11:11 +0100

    fies to phone

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,8 +21,6 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		}
 		fd = open(filename, O_RDWR | O_CREAT, 0600);
-		if (fd == -1)
-			return (-1);
 		close(fd);
 		return (1);
 	}
