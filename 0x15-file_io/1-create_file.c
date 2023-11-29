949c3acbd1c7cbbc079db16ca632c9ac025f3316
memesofting <bibitayoadekunle00@gmail.com>
2023-11-29 17:21:42 +0100

    file creation permision done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,20 @@ int create_file(const char *filename, char *text_content)
 	size_t len;
 
 	if (text_content == NULL)
-		return (-1);
-	len = strlen(text_content);
-	fd = open(filename, O_RDWR, 600);
-	if (fd == -1)
-		return (-1);
-	write(fd, text_content, len);
-	close(fd);
+	{
+		fd = open(filename, O_RDWR | O_CREAT, 0600);
+		close(fd);
+		return (1);
+	}
+	else
+	{
+		len = strlen(text_content);
+		fd = open(filename, O_RDWR | O_CREAT, 0600 | O_TRUNC);
+		if (fd == -1)
+			return (-1);
+		write(fd, text_content, len);
+		close(fd);
+	}
 	return (1);
 
 }
