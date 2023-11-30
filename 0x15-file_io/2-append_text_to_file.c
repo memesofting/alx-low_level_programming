b7f64126b7deecd860341ec585017eb8dd794c8d
memesofting <bibitayoadekunle00@gmail.com>
2023-11-30 09:34:36 +0100

    append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
+ * append_text_to_file - creates a file
  * @filename: name of file to create
  * @text_content: null terminated string to write to file
  *
  * Return: 1 on success
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	size_t len;
@@ -20,19 +20,14 @@ int create_file(const char *filename, char *text_content)
 		{
 			return (-1);
 		}
-		fd = open(filename, O_RDWR | O_CREAT, 0600);
-		close(fd);
 		return (1);
 	}
-	else
-	{
-		len = strlen(text_content);
-		fd = open(filename, O_RDWR | O_CREAT, 0600 | O_TRUNC);
-		if (fd == -1)
-			return (-1);
-		write(fd, text_content, len);
-		close(fd);
-	}
+	len = strlen(text_content);
+	fd = open(filename, O_APPEND);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, len);
+	close(fd);
 	return (1);
 
 }
