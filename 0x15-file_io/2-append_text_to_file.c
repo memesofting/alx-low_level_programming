4f3db967bb123ba7cc468380bb5a9e27f60af83c
memesofting <bibitayoadekunle00@gmail.com>
2023-11-30 10:03:34 +0100

    append file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 	len = strlen(text_content);
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	write(fd, text_content, len);
