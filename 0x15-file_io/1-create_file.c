b837f1fe0f83ab00e386bd860bbeffac80b8df5c
memesofting <bibitayoadekunle00@gmail.com>
2023-11-30 10:49:47 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,15 +26,12 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	else
-	{
-		len = strlen(text_content);
-		fd = open(filename, O_RDWR | O_CREAT, 0600 | O_TRUNC);
-		if (fd == -1)
-			return (-1);
-		write(fd, text_content, len);
-		close(fd);
-	}
+	len = strlen(text_content);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, len);
+	close(fd);
 	return (1);
 
 }
