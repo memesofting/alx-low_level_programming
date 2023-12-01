52f321049b30b1361a9d26fd99abcd1bce744c1a
memesofting <bibitayoadekunle00@gmail.com>
2023-12-01 14:36:30 +0000

    copy file content

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,24 +7,44 @@
  * Return: 1
  */
 
-int copy(char *file_from, char *file_to)
+ssize_t copy(char *file_from, char *file_to)
 {
 	int fdf, fdt;
+	ssize_t rd, wr;
+	char *str;
 
+	str = malloc(sizeof(char) * 1024);
 	fdf = open(file_from, O_RDONLY);
 	if (fdf == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	fdt = open(file_to, O_WRONLY, O_CREAT, 0664 | O_TRUNC);
+	fdt = open(file_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fdt == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
+	rd = read(fdf, str, 1024);
+	if (rd == -1)
+		return (-1);
+	wr = write(fdt, str, 1024);
+	if (wr == -1)
+		return (-1);
 	close(fdf);
+	if (!close(fdf))
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fdf);
+		exit(100);
+	}
 	close(fdt);
+	if (!close(fdt))
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fdt);
+		exit(100);
+	}
+	free(str);
 	return (0);
 }
 
