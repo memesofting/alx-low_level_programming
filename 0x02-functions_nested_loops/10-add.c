d0446aeb4586188d0af5b8308683162e19911846
memesofting <bibitayoadekunle00@gmail.com>
2023-12-12 00:43:21 +0100

    add integers using prototype without parameter name

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @int: integers
+ *
+ * Return: result
+ */
+
+int add(int, int);
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
--- 0x02-functions_nested_loops/meme.c
+++ 0x02-functions_nested_loops/meme.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "meme.h"
 /**
- * entry - point
+ * main - entry point
  *
  * Return: Aways 0 (Success)
  */
 int main(void)
 {
 	int add;
+
 	add = _putchar(2, 4);
 	printf("%d\n", add);
 	return (0);
