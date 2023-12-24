1c055e3f5f2910f2273ed9ab3807a603167f1646
memesofting <bibitayoadekunle00@gmail.com>
2023-12-24 20:22:07 +0000

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_square - prints square array
+ * @size: size of square
+ *
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int l, b;
+
+	b = 1;
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (b <= size)
+		{
+			l = 1;
+			while (l <= size)
+			{
+				_putchar('#');
+				l++;
+			}		
+			_putchar('\n');
+			b++;
+		}
+		_putchar('\n');
+	}
+}
