38252546efc88081e85ebd61ca82d1ee071019de
memesofting <bibitayoadekunle00@gmail.com>
2023-12-28 14:45:06 +0000

    print triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,7 +9,7 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size <= 0)
 	{
@@ -17,10 +17,13 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = size; i >= 1; i--)
+		for (i = 0; i < size; i++)
 		{
-			_putchar(' ');
-			for (j = i; j >= 1; j--)
+			for (j = size - i; j > 1; j--)
+			{
+				_putchar(32);
+			}
+			for (k = 0; k <= i; k++)
 			{
 				_putchar('#');
 			}
