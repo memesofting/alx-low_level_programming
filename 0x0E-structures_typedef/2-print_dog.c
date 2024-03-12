517a93ed9446bf0e04f12dafda5eb2d371856106
memesofting <bibitayoadekunle00@gmail.com>
2024-03-12 16:58:28 +0000

    print dog struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 		return;
 	}
 	(d->name == NULL) ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
-	(d->age == 0) ? printf("Age: (nil)\n") : printf("Age: %f\n", d->age);
+	(d->age == 0) ? printf("Age: 0.000000\n") : printf("Age: %f\n", d->age);
 	(d->owner == NULL) ? printf("Owner: (nil)\n") :
 		printf("Owner: %s\n", d->owner);
 }
