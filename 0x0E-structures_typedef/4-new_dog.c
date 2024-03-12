5674c9ca71cc4434cf6ac30b6a8573eb86a0ec64
memesofting <bibitayoadekunle00@gmail.com>
2024-03-12 20:36:39 +0100

    allocating struct memory

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,14 +18,32 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	name_len = strlen(name);
 	owner_len = strlen(owner);
-	new_d = malloc((sizeof(char) * name_len) +
-			sizeof(float) + (sizeof(char) * owner_len));
+	/*new_d = malloc((sizeof(char) * name_len) +
+			sizeof(float) + (sizeof(char) * owner_len));*/
+	new_d = malloc(sizeof(dog_t));
 	if (new_d == NULL)
 	{
 		return (NULL);
 	}
-	new_d->name = name;
+	new_d->name = malloc((sizeof(char) * name_len) + 1);
+	if (new_d->name == NULL)
+	{
+		free(new_d);
+		return (NULL);
+	}
+	strcpy(new_d->name, name);
+	
+	new_d->owner = malloc((sizeof(char) * owner_len) + 1);
+	if (new_d->owner == NULL)
+	{
+		free(new_d->name);
+		free(new_d);
+		return (NULL);
+	}
+	strcpy(new_d->owner, owner);
+
+	/*new_d->name = name;*/
 	new_d->age = age;
-	new_d->owner = owner;
+	/*new_d->owner = owner;*/
 	return (new_d);
 }
