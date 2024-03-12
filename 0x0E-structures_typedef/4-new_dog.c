0ef085ff1480c4118a5f7326afcc7ffc8a43e362
memesofting <bibitayoadekunle00@gmail.com>
2024-03-12 20:44:20 +0100

    handled aunty betty

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,13 +18,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	name_len = strlen(name);
 	owner_len = strlen(owner);
-	/*new_d = malloc((sizeof(char) * name_len) +
-			sizeof(float) + (sizeof(char) * owner_len));*/
+	/*allocate dog_t memory*/
 	new_d = malloc(sizeof(dog_t));
 	if (new_d == NULL)
 	{
 		return (NULL);
 	}
+	/**
+	 * allocate memory for stings in struct
+	 * and mak copy
+	 */
 	new_d->name = malloc((sizeof(char) * name_len) + 1);
 	if (new_d->name == NULL)
 	{
@@ -32,7 +35,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	strcpy(new_d->name, name);
-	
+
 	new_d->owner = malloc((sizeof(char) * owner_len) + 1);
 	if (new_d->owner == NULL)
 	{
@@ -42,8 +45,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	strcpy(new_d->owner, owner);
 
-	/*new_d->name = name;*/
 	new_d->age = age;
-	/*new_d->owner = owner;*/
+
 	return (new_d);
 }
