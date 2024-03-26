02646a2919d31e2de03da95bb179a894ca9d9001
memesofting <bibitayoadekunle00@gmail.com>
2024-03-26 13:38:01 +0000

    added isupper function to dll

---
+++ 0x18-dynamic_libraries/isupper.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _isupper - checks for lower case
+ * @c: int type for testing _islower
+ *
+ * Return: Always 1 on success
+ * Else 0
+ */
+int _isupper(int c)
+{
+}
