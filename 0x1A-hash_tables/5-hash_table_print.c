4a011cc82d6d1d81ce06dd072ea7d878096a82a0
memesofting <bibitayoadekunle00@gmail.com>
2024-04-18 17:31:09 +0100

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to be printed
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i, comma;
+
+	comma = 0;
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		for (node = ht->array[i]; node != NULL; node = node->next)
+		{
+			if (comma)
+			{
+				printf(", ");
+			}
+			printf("'%s':'%s'", node->key, node->value);
+		}
+	}
+	printf("}\n");
+}
