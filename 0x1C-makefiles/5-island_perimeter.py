78c6a5a6683d7257d3c1fde8196d023830b50814
memesofting <bibitayoadekunle00@gmail.com>
2024-06-06 13:14:11 +0100

    correcting 100-makefile

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Grid island module"""
+
+def island_perimeter(grid):
+    """This method calculates tj=he perimeter
+    of the described island (grid)
+    Return:
+        perimeter of island
+    """
