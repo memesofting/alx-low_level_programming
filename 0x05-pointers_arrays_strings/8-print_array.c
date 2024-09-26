e429510789ed23d9944a8515f06fe789c0c2db5b
memesofting <bibitayoadekunle00@gmail.com>
2024-09-26 16:09:21 +0100

    check for and print array with single element in 8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,18 @@ void print_array(int *a, int n)
 
 	if (a == NULL || n < 0)
 		return;
-	for (i = 0; i < n; i++)
+	if (n == 0)
 	{
-		printf("%d", a[i]);
-		if (i < (n - 1))
-			printf(", ");
+		printf("%d", a[n]);
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", a[i]);
+			if (i < (n - 1))
+				printf(", ");
+		}
 	}
 	printf("\n");
 }
